d30c4caf96b0c08d69e7625e2bc55ed49180db7f
ThibautM <thibaut.marsal@gmail.com>
2018-07-03 23:06:18 +0200

    [ADD] body part clicking

--- feelae/feelae/Controllers/ViewController.swift
+++ feelae/feelae/Controllers/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonsTab: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for button in buttonsTab {
+            emptyButton(button: button)
+            button.setBackgroundImage(UIImage(named: "radio-on-button"), for: .normal)
+            button.addTarget(self, action: #selector(setBackground(_:)), for: .touchUpInside)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @objc func setBackground(_ sender:UIButton!)
+    {
+        for button in buttonsTab {
+            emptyButton(button: button)
+        }
+        print(sender.tag)
+        sender.alpha = 1
+        print(sender)
+    }
+    
+    func emptyButton (button: UIButton) {
+        button.setTitle("", for: .normal)
+        button.alpha = 0.2
+    }
 }
 
